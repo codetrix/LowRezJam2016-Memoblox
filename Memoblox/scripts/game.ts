01b1a0b268e64a0dd06cc37fa1ae14d9ee38eb7f
codetrix <ma3x666@gmail.com>
2016-04-13 20:12:24 +0200

    * Implemented new classes Tile, TileWithBorder and TileFactory * Split code into game.ts and tiles.ts

---
+++ Memoblox/scripts/game.ts
@@ -0,0 +1,94 @@
+﻿const GAME_WIDTH = 64;
+const GAME_HEIGHT = 64;
+
+const GAME_TO_CANVAS_SCALE = 10;
+
+const MIN_CANVAS_WIDTH = GAME_WIDTH;
+const MIN_CANVAS_HEIGHT = GAME_HEIGHT;
+
+const MAX_CANVAS_WIDTH = GAME_WIDTH * GAME_TO_CANVAS_SCALE;
+const MAX_CANVAS_HEIGHT = GAME_HEIGHT * GAME_TO_CANVAS_SCALE;
+
+class SimpleGame {
+
+    private rootGroup: Phaser.Group;
+    private tileFactory: Tiles.TileFactory;
+
+    constructor(gameContainer)
+    {
+        let state = this;
+        let transparent = false;
+        let antialias = false;
+
+        this.game = new Phaser.Game(MAX_CANVAS_WIDTH, MAX_CANVAS_HEIGHT, Phaser.AUTO, gameContainer, state, transparent, antialias);
+    }
+
+    game: Phaser.Game;
+
+    preload()
+    {
+        this.game.load.image('logo', 'assets/phaser_pixel_small_flat.png');
+        this.game.load.image('tile', 'assets/white-1x1.png');
+
+        this.tileFactory = new Tiles.TileFactory(this.game, 'tile');
+        this.tileFactory.size = 8;
+        this.tileFactory.borderSize = 1;
+    }
+
+    create()
+    {
+        // Create a root group that will contain all game entities  
+        this.rootGroup = this.game.add.group();
+
+        this.initScalingLogic();
+
+        let red = Phaser.Color.hexToColor("#ff0000");
+        let green = Phaser.Color.hexToColor("#00ff00");
+
+        //let tile = this.tileFactory.createTileWithBorder(0, 0);
+        let tile = this.tileFactory.createTile(0, 0);
+        //tile.borderTint = green;
+        tile.tileTint = red.color;
+        this.rootGroup.addChild(tile);
+
+        let tile2 = this.tileFactory.createTileWithBorder(8, 0);
+        tile2.borderTint = green.color;
+        tile2.tileTint = red.color;
+        this.rootGroup.addChild(tile2);        
+
+        let logo = this.game.add.sprite(GAME_WIDTH / 2, GAME_HEIGHT / 2, 'logo', 0, this.rootGroup);
+        logo.anchor.setTo(0.5, 0.5);
+    }
+
+    private initScalingLogic()
+    {
+        // Set min and max allowed game window size
+        this.game.scale.setMinMax(MIN_CANVAS_WIDTH, MIN_CANVAS_HEIGHT, MAX_CANVAS_WIDTH, MAX_CANVAS_HEIGHT);
+
+        // Set the scaling mode to show the whole game window when resizing
+        this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+        // Hook resize to update root group layout
+        this.game.scale.setResizeCallback(this.updateRootGroupScale, this);
+
+        // Update root group scale
+        this.updateRootGroupScale();
+    }
+
+    private updateRootGroupScale()
+    {
+        // Calculate the new zoom. Floor calculations to avoid subpixel positioning.
+        let zoom = Math.max(1, Math.floor(this.game.height / GAME_HEIGHT));
+
+        // Floor group position to avoid subpixel placement.
+        this.rootGroup.position.x = Math.floor(this.game.width / 2 - zoom * GAME_WIDTH / 2);
+        this.rootGroup.scale.set(zoom, zoom);
+
+        console.log("Zoom:", zoom);
+    }
+}
+
+window.onload = () =>
+{
+    let game = new SimpleGame('game-container');
+};
---
+++ Memoblox/scripts/tiles.ts
@@ -0,0 +1,70 @@
+﻿module Tiles
+{
+    export class TileFactory {
+        private game: Phaser.Game;
+        private key: string | Phaser.RenderTexture | Phaser.BitmapData | PIXI.Texture;
+
+        public size: number = 8;
+        public borderSize: number = 1;
+
+        constructor(game: Phaser.Game, key: string | Phaser.RenderTexture | Phaser.BitmapData | PIXI.Texture) {
+            this.game = game;
+            this.key = key;
+        }
+
+        createTile(x: number = 0, y: number = 0, size: number = this.size): Tile {
+            return new Tile(this.game, size, x, y, this.key);
+        }
+
+        createTileWithBorder(x: number = 0, y: number = 0, size: number = this.size, borderSize: number = this.borderSize): TileWithBorder {
+            return new TileWithBorder(this.game, size, borderSize, x, y, this.key);
+        }
+    }
+
+    class Tile extends Phaser.Sprite {
+        constructor(game: Phaser.Game, size: number, x: number = 0, y: number = 0, key?: string | Phaser.RenderTexture | Phaser.BitmapData | PIXI.Texture, frame?: string | number) {
+            super(game, x, y, key, frame);
+
+            this.scale.setTo(size);
+        }
+
+        get tileTint() {
+            return this.tint;
+        }
+
+        set tileTint(tint: number) {
+            this.tint = tint;
+        }
+    }
+
+    class TileWithBorder extends Tile {
+        private tile: Tile;
+
+        constructor(game: Phaser.Game, size: number, borderSize: number, x: number = 0, y: number = 0, key?: string | Phaser.RenderTexture | Phaser.BitmapData | PIXI.Texture, frame?: string | number) {
+            super(game, size, x, y, key, frame);
+
+            let innerScale = (size - 2 * borderSize) / size;
+            let innerOffset = borderSize / size;
+
+            this.tile = new Tile(game, innerScale, innerOffset, innerOffset, key, frame);
+
+            this.addChild(this.tile);
+        }
+
+        get tileTint() {
+            return this.tile.tint;
+        }
+
+        set tileTint(tint: number) {
+            this.tile.tint = tint;
+        }
+
+        get borderTint() {
+            return this.tint;
+        }
+
+        set borderTint(tint: number) {
+            this.tint = tint;
+        }
+    }
+}
